92b1d7b805b8782d49a04b5279eb0be8d5dfd8ab
lackoftalentt <leonardokizaru@gmail.com>
2026-09-16 14:20:48 +0500

    implement constructor-based DroneMission

--- src/main/java/DroneMission.java
+++ src/main/java/DroneMission.java
@@ -1,4 +1,4 @@
-package main;
+package main.java;
 
 public class DroneMission {
     private final String missionName;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import main.DroneMission;
+package main.java;
 
 public class Main {
     public static void main(String[] args) {
